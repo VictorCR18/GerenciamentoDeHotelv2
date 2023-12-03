9f61c227ce449aed3ad3899c04567d9871a5a4d1
Victor Cavalcante <cavalcantevictor2003@gmail.com>
2023-12-03 17:35:29 -0300

    Fazendo ajustes

--- src/main/java/br/ufc/quixada/entity/Quarto.java
+++ src/main/java/br/ufc/quixada/entity/Quarto.java
@@ -1,10 +1,8 @@
 package br.ufc.quixada.entity;
 
-import lombok.*;
-
 import jakarta.persistence.*;
 import java.util.List;
-
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Entity
@@ -13,35 +11,53 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-
-@NamedQueries({
-    @NamedQuery(name = "findByTipo", query = "select q from Quarto q where q.tipo = :tipo")
-})
-
+@NamedQueries(
+  {
+    @NamedQuery(
+      name = "findByTipo",
+      query = "select q from Quarto q where q.tipo = :tipo"
+    ),
+  }
+)
 public class Quarto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
 
-    @Column(unique = true, nullable = false)
-    private String capacidade;
-    private String numero;
-    private String tipo;
-    private float precoDaDiaria;
-    private boolean disponivel;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
 
-    @OneToMany(mappedBy = "quarto", cascade = CascadeType.ALL)
-    private List<Reserva> reservas;
+  @Column(unique = true, nullable = false)
+  private String numero;
 
-    @Override
-    public String toString() {
-        return "QuartoId: " + id + ", " +
-                "Numero: " + numero + ", " +
-                "Capacidade: " + capacidade + ", " +
-                "Tipo: " + tipo + ", " +
-                "Preço da Diaria: R$" + precoDaDiaria + ", " +
-                "Disponivel: " + disponivel;
-    }
+  private String tipo;
+  private float precoDaDiaria;
+  private boolean disponivel;
+  private String capacidade;
+
+  @OneToMany(mappedBy = "quarto", cascade = CascadeType.ALL)
+  private List<Reserva> reservas;
+
+  @Override
+  public String toString() {
+    return (
+      "QuartoId: " +
+      id +
+      ", " +
+      "Numero: " +
+      numero +
+      ", " +
+      "Capacidade: " +
+      capacidade +
+      ", " +
+      "Tipo: " +
+      tipo +
+      ", " +
+      "Preço da Diaria: R$" +
+      precoDaDiaria +
+      ", " +
+      "Disponivel: " +
+      disponivel
+    );
+  }
 
   public boolean temReserva() {
     return reservas != null && !reservas.isEmpty();
